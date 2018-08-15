42b090284c493959b5ef07696bf08df81c311cf8
Matt Coster <matthew@thecosters.co.uk>
2018-08-15 23:27:05 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -4,7 +4,7 @@ html_theme_path = guzzle_sphinx_theme.html_theme_path()
 html_theme = 'guzzle_sphinx_theme'
 
 # Register the theme as an extension to generate a sitemap.xml
-extensions.append("guzzle_sphinx_theme")
+extensions = ["guzzle_sphinx_theme"]
 
 # Guzzle theme options (see theme.conf for more information)
 html_theme_options = {
