67b927a7983a75c157b43821a05cf5e47d556721
Matt Coster <matthew@thecosters.co.uk>
2018-08-15 23:16:57 +0100

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,13 @@
+import guzzle_sphinx_theme
+
+html_theme_path = guzzle_sphinx_theme.html_theme_path()
+html_theme = 'guzzle_sphinx_theme'
+
+# Register the theme as an extension to generate a sitemap.xml
+extensions.append("guzzle_sphinx_theme")
+
+# Guzzle theme options (see theme.conf for more information)
+html_theme_options = {
+    # Set the name of the project to appear in the sidebar
+    "project_nav_name": "Docs Test",
+}
