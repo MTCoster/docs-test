3169e56848d4c4c6204ffc12622495aa37dcf605
Matt Coster <matthew@thecosters.co.uk>
2018-08-21 11:56:17 +0100

    shinx -> sphinx typo fix mk 2

--- docs/conf.py
+++ docs/conf.py
@@ -16,7 +16,7 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
-import shinx-rtd-theme
+import sphinx-rtd-theme
 
 # -- Project information -----------------------------------------------------
 
